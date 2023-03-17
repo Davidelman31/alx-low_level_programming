579506ffd9edb8808f8d747463bd6f96b03fabeb
Davidelman31 <davidelman31@gmail.com>
2023-03-17 10:01:07 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
         char alp[26] = "abcdefghijklmnopqrstuvwxyz";
         int i;
 
@@ -17,14 +16,4 @@ int main(void)
         }
         putchar('\n');
         return (0);
-=======
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
->>>>>>> 26e7c4e48a5314122ddfc5fd780529ed649be77b
 }
