2cc91356dc1f488ad2ac306a9bc39a97cd21a4b2
Davidelman31 <davidelman31@gmail.com>
2023-03-23 08:03:24 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,9 +4,9 @@ int _isupper(int c);
 
 int _isdigit(int c);
 
-int _mul(int a, int b);
+int mul(int a, int b);
 
-void _print_numbers(void);
+void print_numbers(void);
 
 void print_most_numbers(void);
 
